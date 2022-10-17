040307883343f86385d7950984a13efe10245bc0
Luke <72365181+snazzybytes@users.noreply.github.com>
2022-10-17 03:56:46 -0500

    [functional-handler-tweak] fix functional Handler to return PriceInfo instead (#4)

--- src/main/java/com/snazzybytes/reactiverestapi/functional/PriceRequestHandler.java
+++ src/main/java/com/snazzybytes/reactiverestapi/functional/PriceRequestHandler.java
@@ -1,5 +1,8 @@
 package com.snazzybytes.reactiverestapi.functional;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
@@ -7,6 +10,7 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import com.snazzybytes.reactiverestapi.dto.PriceInfo;
 import com.snazzybytes.reactiverestapi.dto.coinbase.PriceData;
 import com.snazzybytes.reactiverestapi.service.CoinbaseWebClient;
 
@@ -28,11 +32,19 @@ public class PriceRequestHandler {
         watch.start();
         final String currencyCode = request.pathVariable("currencyCode");
         log.info("Processing request for currencyCode={}", currencyCode);
-        Mono<PriceData> priceDataMono = webClient.getBtcPrice(currencyCode);
+        final Mono<PriceData> priceDataMono = webClient.getBtcPrice(currencyCode);
         return priceDataMono.flatMap(
-                priceData -> ServerResponse.ok()
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body(BodyInserters.fromValue(priceData)));
+                priceData -> {
+                    final BigDecimal amount = new BigDecimal(priceData.getAmount()).setScale(2, RoundingMode.HALF_UP);
+                    return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(
+                            BodyInserters.fromValue(PriceInfo.builder()
+                                    .price(amount)
+                                    .currency(priceData.getCurrency())
+                                    // calculate sats per fiat
+                                    .satsPerFiat(
+                                            BigDecimal.valueOf(100000000L).divide(amount, RoundingMode.HALF_UP))
+                                    .build()));
+                });
     }
 
 }
